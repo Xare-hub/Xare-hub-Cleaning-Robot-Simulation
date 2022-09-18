1f7b15525d99aea00091816e2e56dbf68e4e5709
Javier Rojas <javier990507@gmail.com>
2022-09-18 12:49:06 -0500

    Deleted unused file

--- Week 4/Canvas_and_Drawing.py
+++ Week 4/Canvas_and_Drawing.py
@@ -4,14 +4,22 @@ import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
 # define draw handler
 def draw(canvas):
-    canvas.draw_text("Hello!",[100, 100], 24, "White")
-    canvas.draw_circle([100, 100], 2, 2, "Red")
+    canvas.draw_circle([100, 100], 50, 2, "Red", "Pink")
+    canvas.draw_circle([300, 300], 50, 2, "Red", "Pink")
+    canvas.draw_line([100, 100],[300, 300], 2, "Black")
+    canvas.draw_circle([100, 300], 50, 2, "Green", "Lime")
+    canvas.draw_circle([300, 100], 50, 2, "Green", "Lime")
+    canvas.draw_line([100, 300],[300, 100], 2, "Black")
+    canvas.draw_polygon([[150, 150], [250, 150], [250, 250], [150, 250]], 2, 
+          "Blue", "Aqua")
+    canvas.draw_text("An example of drawing", [60, 385], 24, "Black")
 
 # create frame
-frame = simplegui.create_frame("Text drawing", 300, 200)
+frame = simplegui.create_frame("Text drawing", 400, 400)
 
 # register draw handler    
 frame.set_draw_handler(draw)
+frame.set_canvas_background("Yellow")
 
 # start frame
 frame.start()
